38a0df34f5d34f738e32b3b4ab4842acf9e8061c
swaraj <121567727+Swaraj-1925@users.noreply.github.com>
2024-12-10 15:34:05 +0530

    Update keymaps.lua
    Changed keymaps related to tab

--- nvim/lua/swaraj/core/keymaps.lua
+++ nvim/lua/swaraj/core/keymaps.lua
@@ -23,8 +23,27 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })                    -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })               -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
-keymap.set("n", "<A-x>", "<cmd>tabclose<CR>", { desc = "Close current tab" })                   -- close current tab
-keymap.set("n", "<C-k>", "<cmd>tabn<CR>", { desc = "Go to next tab" })                          --  go to next tab
-keymap.set("n", "<C-l>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                      --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<C-n>", "<cmd>tabnew<CR>", { desc = "Open new tab" })            -- open new tab
+keymap.set("n", "<C-x>", "<cmd>tabclose<CR>", { desc = "Close current tab" })     -- close current tab
+
+--keymap.set("n", "<C-l>", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
+-- Cyclical next tab
+keymap.set("n", "<C-l>", function()
+    if vim.fn.tabpagenr() == vim.fn.tabpagenr("$") then
+        vim.cmd("tabfirst") -- Go to the first tab if on the last tab
+    else
+        vim.cmd("tabnext")  -- Go to the next tab
+    end
+end, { desc = "Go to next tab (cyclical)" })
+
+--keymap.set("n", "<C-k>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
+-- Cyclical previous tab
+keymap.set("n", "<C-k>", function()
+    if vim.fn.tabpagenr() == 1 then
+        vim.cmd("tablast")     -- Go to the last tab if on the first tab
+    else
+        vim.cmd("tabprevious") -- Go to the previous tab
+    end
+end, { desc = "Go to previous tab (cyclical)" })
+
+keymap.set("n", "<C-o>", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
